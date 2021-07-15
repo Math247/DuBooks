6a5bea0b1d2e63858fb6cf26b4f10994422bae1f
fmmarmelloBucket <fmmarmello@gmail.com>
2021-07-15 16:49:31 -0300

    alteração provisória de código para envio de anexo

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,4 +1,11 @@
-<?
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
 $telefone			= $_POST["telefone"];	// Pega o valor do campo Telefone
 $email				= $_POST["email"];	// Pega o valor do campo Email
 $formato    		= $_POST["formato"];
@@ -18,51 +25,80 @@ $data 				= date("d/m/Y H:i:s ");
 
 // Variável que junta os valores acima e monta o corpo do email
 
-		$Vai  = '<html><body>';
-		$Vai .= '<table rules="all" style="border: bgcolor="#000000" frame="box"" cellpadding="10">';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Email:</strong> </td><td>' . $email . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Telefone:</strong> </td><td>' . $telefone . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Formato:</strong> </td><td>' . $formato . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Papel de Miolo:</strong> </td><td>' . $papelmiolo . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Papel de Capa:</strong> </td><td>' . $papeldecapa . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Acabamento de Capa:</strong> </td><td>' . $acabamentodecapa . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Acabamento do Livro:</strong> </td><td>' . $acabamentodolivro . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Quantidade de páginas coloridas:</strong> </td><td>' . $qtdpagcoloridas . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Quantidade de páginas preto e branco:</strong> </td><td>' . $qtdpagpretobranco . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Total de Páginas:</strong> </td><td>' . $totpag . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Tiragem:</strong> </td><td>' . $tiragem . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Mensagem:</strong> </td><td>' . $mensagem . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Enviado do site em:</strong> </td><td>' . $data . '</td></tr>';
-		$Vai .= '<tr><td bgcolor="#C0C0C0"><strong>Nome do Arquivo:</strong> </td><td>' . $arquivo_nome . '</td></tr>';
-		$Vai .= '</table>';
-		$Vai .= '</body></html>';
+$body  = '<html><body>';
+$body .= '<table rules="all" style="border: bgcolor="#000000" frame="box"" cellpadding="10">';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Email:</strong> </td><td>' . $email . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Telefone:</strong> </td><td>' . $telefone . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Formato:</strong> </td><td>' . $formato . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Papel de Miolo:</strong> </td><td>' . $papelmiolo . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Papel de Capa:</strong> </td><td>' . $papeldecapa . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Acabamento de Capa:</strong> </td><td>' . $acabamentodecapa . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Acabamento do Livro:</strong> </td><td>' . $acabamentodolivro . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Quantidade de páginas coloridas:</strong> </td><td>' . $qtdpagcoloridas . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Quantidade de páginas preto e branco:</strong> </td><td>' . $qtdpagpretobranco . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Total de Páginas:</strong> </td><td>' . $totpag . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Tiragem:</strong> </td><td>' . $tiragem . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Mensagem:</strong> </td><td>' . $mensagem . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Enviado do site em:</strong> </td><td>' . $data . '</td></tr>';
+$body .= '<tr><td bgcolor="#C0C0C0"><strong>Nome do Arquivo:</strong> </td><td>' . $arquivo_nome . '</td></tr>';
+$body .= '</table>';
+$body .= '</body></html>';
                 
-                
-require_once("phpmailer/class.phpmailer.php");
-
 define('GUSER', 'noreplay.dooboks@gmail.com');	// <-- Insira aqui o seu GMail
 define('GPWD', 's3nh@n0v@');		// <-- Insira aqui a senha do seu GMail
+define('FROM_NAME', 'Produtora DuBooks');		// <-- Insira aqui a senha do seu GMail
 
-function smtpmailer($para, $copia, $copiaoculta, $de, $de_nome, $assunto, $corpo) { 
+function smtpmailer($para, $copia, $copiaoculta, $de, $de_nome, $assunto, $corpo,$arquivo_caminho,$arquivo_nome) { 
 	global $error;
 	$mail = new PHPMailer();
-	$mail->IsHTML(true);
-	$mail->CharSet = "utf-8";
-	$mail->IsSMTP();		// Ativar SMTP
-	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
-	$mail->SMTPAuth = true;		// Autenticação ativada
-	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
-	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
-	$mail->Port = 465;  		// A porta 465 deverá estar aberta em seu servidor
+	$mail->isSMTP();
+
+	//Enable SMTP debugging
+	//SMTP::DEBUG_OFF = off (for production use)
+	//SMTP::DEBUG_CLIENT = client messages
+	//SMTP::DEBUG_SERVER = client and server messages
+	$mail->SMTPDebug = SMTP::DEBUG_OFF;
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+	$mail->SMTPAutoTLS = false;
+
+	$mail->Port = 587;
+
+	//Set the encryption mechanism to use:
+	// - SMTPS (implicit TLS on port 465) or
+	// - STARTTLS (explicit TLS on port 587)
+	$mail->SMTPSecure = 'tls';
+
+	$mail->SMTPAuth = true;
 	$mail->Username = GUSER;
 	$mail->Password = GPWD;
-	$mail->SetFrom($de, $de_nome);
+
+	$mail->setFrom(GUSER, 'First Last');
+	$mail->addReplyTo('fmmarmello@gmail.com', 'First Last');//responder para (email do usuario que enviou)
+	$mail->addAddress($para, 'John Doe');//PARA
+	// $mail->addCC($copia);
+    // $mail->addBCC($copiaoculta);
+
 	$mail->Subject = $assunto;
+
+	//Read an HTML message body from an external file, convert referenced images to embedded,
+	//convert HTML into a basic plain-text alternative body
+	//$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
 	$mail->Body = $corpo;
-	$mail->AddAddress($para);
-	$mail->AddBcc($copia);
-	$mail->AddCc($copiaoculta);
-	$mail->AddAttachment($arquivo_caminho, $arquivo_nome); //anexando arquivo
+
+	//Replace the plain text body with one created manually
+	$mail->AltBody = 'Corpo alternativo';
+
+	//Attach an image file
+	$mail->addAttachment($arquivo_caminho,$arquivo_nome);
+
+	//send the message, check for errors
 	if(!$mail->Send()) {
 		$error = 'Mail error: '.$mail->ErrorInfo; 
 		return false;
@@ -72,13 +108,17 @@ function smtpmailer($para, $copia, $copiaoculta, $de, $de_nome, $assunto, $corpo
 	}
 }
 
+
+
+
 // Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
 // o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
 
- if (smtpmailer('matheuschabib@gmail.com', 'gushabib@live.com', 'gustavo@qrc.com.br', 'GUSER', 'Formulario do Site', 'DoBooks - Contato pelo site', $Vai)) {
+ //if (smtpmailer('matheuschabib@gmail.com', 'gushabib@live.com', 'gustavo@qrc.com.br', 'GUSER', 'Formulario do Site', 'DoBooks - Contato pelo site', $body)) {
+ if (smtpmailer('fmmarmello@gmail.com', '', '', GUSER, FROM_NAME, 'DoBooks - Contato pelo site', $body,$arquivo_caminho,$arquivo_nome)) {
 
 	Header("location: ../../index.html"); // Redireciona para uma página de obrigado.
 
 }
-if (!empty($error)) echo $error;
+//if (!empty($error)) echo $error;
 ?>
